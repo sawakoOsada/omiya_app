91a6c93daf90a135044629911f597595ba7eb88c
sawakoOsada <mugiwaranana@yahoo.co.jp>
2021-02-14 18:42:19 +0000

    add:addressモデルテスト実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,7 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :name, presence: true
   validates :zip, presence: true, length: { is: 7 }, numericality: { only_integer: true }
+  validates :prefecture_id, presence: true
   validates :city, presence: true
   validates :town, presence: true
   belongs_to_active_hash :prefecture
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,11 +1,43 @@
-# require 'rails_helper'
-# RSpec.describe '住所モデル機能', type: :model do
-#
-#   let!(:user) { FactoryBot.create(:user) }
-#
-#   describe 'バリデーションのテスト' do
-#
-#     context 'レビューに星とコメントがある場合' do
-#       it '有効である' do
-#         address = FactoryBot.build(:address, user: user)
-#         expect(address).to be_valid
+require 'rails_helper'
+RSpec.describe '住所モデル機能', type: :model do
+
+  let!(:user) { FactoryBot.create(:user) }
+
+  describe 'バリデーションのテスト' do
+
+    context '氏名、住所がある場合' do
+      it '有効である' do
+        address = FactoryBot.build(:address, user: user)
+        expect(address).to be_valid
+      end
+    end
+
+    context '郵便番号がない場合' do
+      it '無効である' do
+        address = FactoryBot.build(:address, user: user, zip: nil)
+        expect(address).not_to be_valid
+      end
+    end
+
+    context '県がない場合' do
+      it '無効である' do
+        address = FactoryBot.build(:address, user: user, prefecture_id: nil)
+        expect(address).not_to be_valid
+      end
+    end
+
+    context '市区町村がない場合' do
+      it '無効である' do
+        address = FactoryBot.build(:address, user: user, city: nil)
+        expect(address).not_to be_valid
+      end
+    end
+
+    context '町域がない場合' do
+      it '無効である' do
+        address = FactoryBot.build(:address, user: user, town: nil)
+        expect(address).not_to be_valid
+      end
+    end
+  end
+end
