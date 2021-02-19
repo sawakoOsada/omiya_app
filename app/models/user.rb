c95418d7de86bf8fd6d47d79cee5235cd58e71f3
sawakoOsada <mugiwaranana@yahoo.co.jp>
2021-02-19 19:30:19 +0000

    add:住所補完機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   has_many :addresses, dependent: :destroy
   accepts_nested_attributes_for :addresses, reject_if: :reject_addresses, allow_destroy: true
   def reject_addresses(attributed)
-    attributed['name'].blank?
+    attributed['name'].blank? &&
+    attributed['zip'].blank? &&
+    attributed['prefecture_id'].blank? &&
+    attributed['city'].blank? &&
+    attributed['town'].blank?
   end
 end
